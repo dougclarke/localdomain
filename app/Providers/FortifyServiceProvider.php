fc89434d71b8259d9eeefd611bf69162fcbd8697
DC <doug@dougclarke.ca>
2021-03-01 01:04:33 -0500

    Require email confirmation

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -32,5 +32,6 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+        Fortify::verifyEmailView(fn () => view('auth.verify-email'));
     }
 }
